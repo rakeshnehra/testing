1565d32bbcec471d38047c3dd6d0cea9b8370a3b
rakesh <rakesh>
2023-03-31 19:12:04 +0530

    php data add

---
+++ loop/loop.php
@@ -0,0 +1,25 @@
+
+
+<?php  
+
+$x = 1;
+ 
+while($x <= 5) {
+  echo "The number is: $x <br>";
+  $x++;
+} 
+
+$x = 1;
+ 
+while($x <= 5) {
+  echo "The number is: $x <br>";
+  $x++;
+} 
+$x = 1;
+ 
+while($x <= 5) {
+  echo "The number is: $x <br>";
+  $x++;
+} 
+?>  
+
